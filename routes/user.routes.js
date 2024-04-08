1c191fbd8e749f2f74fed439fead3b0f71b2db0b
DevSon0812 <97348196+DevSon0812@users.noreply.github.com>
2024-04-08 12:35:05 +0700

    update

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,10 +2,10 @@ import express from "express";
 import userController from "../controllers/user.controller.js";
 const router = express.Router();
 
-router.get("/", userController.getAllusers);
-router.get("/:id", userController.getuserdetail);
-router.post("/", userController.createuser);
-router.put("/:id", userController.updateuser);
-router.delete("/:id", userController.deleteuser);
+router.get("/", userController.getAllUsers);
+router.get("/:id", userController.getUserdetail);
+router.post("/", userController.createUser);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
 
 export default router;
