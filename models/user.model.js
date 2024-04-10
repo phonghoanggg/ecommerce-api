2410be2425bd63ab1891229a47d13c44cb45e431
DevSon0812 <97348196+DevSon0812@users.noreply.github.com>
2024-04-10 10:15:12 +0700

    update

--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  username: {
+  firstName: {
     type: String,
     required: true,
     unique: true,
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  lastname: {
+  lastName: {
     type: String,
   },
   role: {
