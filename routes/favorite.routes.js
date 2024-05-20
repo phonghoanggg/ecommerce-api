eaebb2f66d3c27d6ddd6e766f9df17a1ba5f9ae4
DevSon0812 <97348196+DevSon0812@users.noreply.github.com>
2024-05-21 00:44:35 +0700

    update

--- routes/favorite.routes.js
+++ routes/favorite.routes.js
@@ -2,12 +2,14 @@ import express from "express";
 import {
   addFavoriteProduct,
   getFavoriteProductsByUser,
+  removeFavoriteProduct,
 } from "../controllers/favorite.controller.js";
 
 const router = express.Router();
 
 // Định nghĩa route để thêm sản phẩm vào danh sách yêu thích của người dùng
 router.post("/", addFavoriteProduct);
+router.delete("/productId", removeFavoriteProduct);
 
 // Định nghĩa route để lấy danh sách sản phẩm yêu thích của người dùng
 router.get("/:userId", getFavoriteProductsByUser);
